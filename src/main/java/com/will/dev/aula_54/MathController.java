2adb73a0d70e80fd87245928c64446487fdcf87b
Wilson <wilsonprogram@gmail.com>
2023-05-30 18:20:49 -0300

    Aula 55 - Definindo a lógica da nossa aplicação

--- src/main/java/com/will/dev/aula_54/MathController.java
+++ src/main/java/com/will/dev/aula_54/MathController.java
@@ -15,10 +15,22 @@ public class MathController {
 	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
 	public Double sum(
 		@PathVariable(value = "numberOne") String numberOne,
-		@PathVariable(value = "numberTwo") String numberTwo)
+		@PathVariable(value = "numberTwo") String numberTwo) throws Exception
 	{
-		
-		return 1D;
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new Exception();
+		}
+		return (convertToDouble(numberOne) + convertToDouble(numberTwo));
+	}
+
+	private Double convertToDouble(String numberOne) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private boolean isNumeric(String strNumber) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
 	
