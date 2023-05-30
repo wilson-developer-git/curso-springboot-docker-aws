f415801ecf5813b975ded2288065337b3817a939
Wilson <wilsonprogram@gmail.com>
2023-05-30 15:12:16 -0300

    Aula 48 - Criando o primeiro Endpoint REST

---
+++ src/main/java/com/will/dev/Greeting.java
@@ -0,0 +1,24 @@
+package com.will.dev;
+
+public class Greeting {
+
+	private Long id;
+	private String content;
+	
+	
+	
+	public Greeting(Long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public String getContent() {
+		return content;
+	}
+	
+	
+	
+}
